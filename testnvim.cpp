e77c42bc63c279fb543d0e9acfc09f11365acf8e
Mirai Kuriyama <huuhoag1412@gmail.com>
2022-01-06 10:02:21 +0700

    a

--- testnvim.cpp
+++ testnvim.cpp
@@ -1,39 +1,38 @@
 #include <stdio.h>
-#include <math.h>
-
-int nhapnguyenduong(){
-	float n;
-	printf("\nHay nhap so nguyen duong n: ");
-	scanf("%f", &n);
-	while (n<=0 || ((int) n)*1.0 != n)
-	{
-		printf("Nhap lai n: ");
-		scanf("%f",&n);
-	}
-
- 	return (int)n;
+void nhapmang(int a[],int n){
+    for(int i=0;i<n;i++){
+        printf("\nNhap gia tri a[%d]= ",i);
+        scanf("%d",&a[i]);
+    }
 }
-
-void phantachthanhSNT(int &n){
-	for(int i=2;i<=n;i++){
-		int dem = 0;
-		while(n%i==0){
-			++dem;
-			n=n/i;
-		}
-		if(dem){
-			if(dem>1) printf ("%d^%d*",i,dem);
-			else printf("%d*",i);
-			if(n>i)
-				printf("%d*",i);
-		}
-	}
+void xuatmang(int a[],int n){
+    for(int i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
 }
-
-int main(int argc,char **argv){
-	
-	int n = nhapnguyenduong();
-	printf("%d",n);
-	phantachthanhSNT(n);
-	return 0;
+int daonguoc(int n)
+{          
+   int sdn=0;
+   while (n!=0)
+   {
+      sdn=sdn*10+n%10;
+      n=n/10;
+   }
+   return sdn;
+}
+int soganhdautien(int a[], int n)
+{
+   for (int i=0; i<n; i++){
+	   if (daonguoc(a[i]) == a[i]){
+		   return 1;
+	   }     
+   }
+      
+   return 0;
+}
+int main(){
+	int a[100];
+	int n; scanf("%d", &n);
+	nhapmang(a,n);
+	soganhdautien(a,n);
 }
